66961dc13d38107e7e00bb35bf98895312721985
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2024-12-18 03:23:50 +0900

    refactor: DTO 클래스 타입 변경

--- src/main/java/com/ani/taku_backend/global/exception/ExceptionDto.java
+++ src/main/java/com/ani/taku_backend/global/exception/ExceptionDto.java
@@ -1,22 +1,16 @@
 package com.ani.taku_backend.global.exception;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
 
-@Getter
-public class ExceptionDto {
-    @NotNull
-    private final Integer code;
-
-    @NotNull
-    private final String message;
-
-    public ExceptionDto(ErrorCode errorCode) {
-        this.code = errorCode.getCode();
-        this.message = errorCode.getMessage();
-    }
+public record ExceptionDto (
+        @NotNull Integer code,
+        @NotNull String message
+) {
 
     public static ExceptionDto of(ErrorCode errorCode) {
-        return new ExceptionDto(errorCode);
+        return new ExceptionDto(
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
     }
 }
