a8bd56b8933c25342a8eb750de9b53042683cb0d
looco <ses1238@gmail.com>
2024-12-18 00:17:43 +0900

    커뮤니티 제외 , 카테고리 관련 릴레이션 추가 및 수정

---
+++ src/main/java/com/ani/taku_backend/user/model/entity/UserImage.java
@@ -0,0 +1,37 @@
+package com.ani.taku_backend.user.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 유저 프로필 이미지 엔티티
+ */
+@Entity
+@Table(name = "user_images")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image image;
+
+
+}
