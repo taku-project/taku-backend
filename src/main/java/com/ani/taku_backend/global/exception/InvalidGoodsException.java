8b3d7e4794de09369db6e40cb356627dcace1430
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2024-12-18 03:12:25 +0900

    feat(*): 단일, 다수 응답 처리를 ApiResponse에서 처리하도록 수정

--- src/main/java/com/ani/taku_backend/global/exception/InvalidGoodsException.java
+++
@@ -1,5 +0,0 @@
-package com.ani.taku_backend.global.exception;
-
-public class InvalidGoodsException extends RuntimeException{
-    public InvalidGoodsException() {super();}
-}
--- src/main/java/com/ani/taku_backend/global/exception/InvalidUserExcepetion.java
+++
@@ -1,5 +0,0 @@
-package com.ani.taku_backend.global.exception;
-
-public class InvalidUserExcepetion extends RuntimeException{
-    public InvalidUserExcepetion(){super();}
-}
