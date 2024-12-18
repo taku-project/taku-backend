5a9b1c663d2ae83171e57d606e1c8b0e688400e4
nagul2 <hayani0000@gmail.com>
2024-12-18 20:03:32 +0900

    PostService 작성

--- src/main/java/com/ani/taku_backend/post/service/PostService.java
+++ src/main/java/com/ani/taku_backend/post/service/PostService.java
@@ -1,9 +1,12 @@
 package com.ani.taku_backend.post.service;
 
+import com.ani.taku_backend.post.model.entity.Post;
 import com.ani.taku_backend.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,5 +15,7 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-
+    public List<Post> getPosts(String filter, Object lastValue, boolean isAsc, int limit) {
+        return postRepository.findPostsWithNoOffset(filter, lastValue, isAsc, limit);
+    }
 }
