bb6a9dafc08fa374c50713511585798457437d67
nagul2 <hayani0000@gmail.com>
2024-12-18 14:20:54 +0900

    쿼리 dsl 리포지토리 연결

--- src/main/java/com/ani/taku_backend/post/repository/PostRepository.java
+++ src/main/java/com/ani/taku_backend/post/repository/PostRepository.java
@@ -1,7 +1,8 @@
 package com.ani.taku_backend.post.repository;
 
 import com.ani.taku_backend.post.model.entity.Post;
+import com.ani.taku_backend.post.repository.impl.PostRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
