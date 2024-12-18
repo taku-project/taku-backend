39b19160272e7c4b67bfb8974c9f76338e8f0598
nagul2 <hayani0000@gmail.com>
2024-12-18 19:42:52 +0900

    Post 클래스 이미지 필드 연관 주석처리 -> 임시

--- src/main/java/com/ani/taku_backend/post/model/entity/Post.java
+++ src/main/java/com/ani/taku_backend/post/model/entity/Post.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.joda.time.DateTime;
 
+import java.time.LocalDateTime;
+
 
 @Table(name = "posts")
 @Entity
@@ -27,10 +29,33 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     private Category category;
 
+    // Image Entity 푸쉬 되면 일대다 다대일 연관관계 매핑 적용
+//    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Image> images = new ArrayList<>();
+
     private String title;
     private String content;
 
-    private DateTime createdAt;
-    private DateTime updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    private int views;
+    private int likes;
+
+    // 조회수 증가
+    public void addViews() {
+        this.views++;
+    }
+
+    // 좋아요 수 증가
+    public void addLikes() {
+        this.likes++;
+    }
+
+    // 좋아요 수 감소
+    public void subLikes() {
+        if (this.likes > 0) {
+            this.likes--;
+        }
+    }
 }
