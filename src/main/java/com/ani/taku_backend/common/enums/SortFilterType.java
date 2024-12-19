7934b4f2908b6ad45c49ea24161819637f47f02c
nagul2 <hayani0000@gmail.com>
2024-12-19 19:08:23 +0900

    정렬 기준 필터 Enum 생성

---
+++ src/main/java/com/ani/taku_backend/common/enums/SortFilterType.java
@@ -0,0 +1,51 @@
+package com.ani.taku_backend.common.enums;
+
+/**
+ * 정렬 기준을 관리
+ */
+public enum SortFilterType {
+
+    /**
+     * 최신순 - 커뮤니티 글, 상품 적용
+     */
+    LATEST("latest"),
+
+    /**
+     * 조회수 기준 - 커뮤니티 글 적용
+     */
+    VIEWS("views"),
+
+    /**
+     * 좋아요 기준 - 커뮤니티 글 적용
+     */
+    LIKES("likes"),
+
+    /**
+     * 가격 내림차순 - 상품 적용
+     */
+    PRICE_DESC("price_desc"),
+
+    /**
+     * 가격 오름차순 - 상품 적용
+     */
+    PRICE_ASC("price_asc");
+
+    private final String value;
+
+    /**
+     * 생성자
+     * @param value 필터 기준 문자열 값
+     *
+     */
+    SortFilterType(String value) {
+        this.value = value;
+    }
+
+    /**
+     * 기준 반환
+     * @return 필터 기준 문자열
+     */
+    public String getValue() {
+        return value;
+    }
+}
