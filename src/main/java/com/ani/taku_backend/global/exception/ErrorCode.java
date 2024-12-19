a34033b32685ae27b995f36fd51a22f01d97bfbe
looco <ses1238@gmail.com>
2024-12-19 19:01:10 +0900

    API예외를 위한 코드 추가

--- src/main/java/com/ani/taku_backend/global/exception/ErrorCode.java
+++ src/main/java/com/ani/taku_backend/global/exception/ErrorCode.java
@@ -13,8 +13,13 @@ public enum ErrorCode {
     NOT_FOUND_END_POINT(40400, HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    INVALIDATE_IMAGE_UPLOAD(50001, HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 실패"),
+
     INVALIDATE_USER(1000,HttpStatus.NOT_FOUND,"해당하는 사용자가 없습니다."),
-    INVALIDATE_GOODS(1001,HttpStatus.NOT_FOUND,"해당하는 상품이 존재하지 않습니다.");
+    INVALIDATE_GOODS(1001,HttpStatus.NOT_FOUND,"해당하는 상품이 존재하지 않습니다."),
+
+    BLACK_USER(4001,HttpStatus.UNAUTHORIZED,"블랙 유저는 카테고리 생성을 할 수 없습니다."),
+    INVALIDATE_IMAGE(4002,HttpStatus.BAD_REQUEST,"이미지 파일이 아닙니다.");
 
     private final Integer code ;
     private final HttpStatus httpStatus;
