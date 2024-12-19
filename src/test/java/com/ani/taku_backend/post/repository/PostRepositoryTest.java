c3624da7888733a2e5b53c70a6ba9b7d4b3cdb17
nagul2 <hayani0000@gmail.com>
2024-12-19 23:11:50 +0900

    test 코드 수정

--- src/test/java/com/ani/taku_backend/post/repository/PostRepositoryTest.java
+++ src/test/java/com/ani/taku_backend/post/repository/PostRepositoryTest.java
@@ -25,21 +25,37 @@ class PostRepositoryTest {
     @Transactional
     @Commit
     void saveTestData() {
-        int totalRecords = 1000000;
+        int totalRecords = 200000;
         int batchSize = 100000;
 
         for (int i = 1; i <= totalRecords; i++) {
-            int views = ThreadLocalRandom.current().nextInt(1, 300000);
-            int likes = ThreadLocalRandom.current().nextInt(1, 100000);
+            Long views = ThreadLocalRandom.current().nextLong(1, 300000);
+            Long likes = ThreadLocalRandom.current().nextLong(1, 100000);
 
             LocalDateTime createdTime = LocalDateTime.now().minusSeconds(totalRecords - i);
             LocalDateTime updatedTime = createdTime.plusSeconds(ThreadLocalRandom.current().nextInt(0, 60));
 
             // 데이터 생성
-            Post post = new Post(null, null, null, "title" + i, "content" + i,
+            Post post1 = new Post(null, null, null, "title" + i, "content" + i,
+                                    createdTime, updatedTime, views, likes);
+
+            Post post2 = new Post(null, null, null, "제목" + i, "내용" + i,
+                                    createdTime, updatedTime, views, likes);
+
+            Post post3 = new Post(null, null, null, "사랑" + i, "평화" + i,
                     createdTime, updatedTime, views, likes);
 
-            entityManager.persist(post);
+            Post post4 = new Post(null, null, null, "진실" + i, "거짓" + i,
+                    createdTime, updatedTime, views, likes);
+
+            Post post5 = new Post(null, null, null, "very" + i, "good" + i,
+                    createdTime, updatedTime, views, likes);
+
+            entityManager.persist(post1);
+            entityManager.persist(post2);
+            entityManager.persist(post3);
+            entityManager.persist(post4);
+            entityManager.persist(post5);
 
             // 배치 처리
             if (i % batchSize == 0) {
@@ -66,9 +82,10 @@ class PostRepositoryTest {
 
     private void likesFilterPagingTest(Post findPost) {
         long startTime = System.nanoTime();
-        List<Post> result = postRepository.findAllPostWithNoOffset("likes", findPost.getLikes(), false, 20, null);
+        List<Post> result = postRepository.findAllPostWithNoOffset("likes", findPost.getLikes(), false, 20, "제목");
         long resultTime = (System.nanoTime() - startTime) / 1000000;
 
+        System.out.println("lastValue = " + findPost.getLikes());
         System.out.println("resultTime(likes) = " + resultTime + "ms");
         for (Post post : result) {
             System.out.println("post.getId() = " + post.getId() + " post.getLikes() = " + post.getLikes());
@@ -77,9 +94,10 @@ class PostRepositoryTest {
 
     private void viewsFilterPagingTest(Post findPost) {
         long startTime = System.nanoTime();
-        List<Post> result = postRepository.findAllPostWithNoOffset("views", findPost.getViews(), false, 20, null);
+        List<Post> result = postRepository.findAllPostWithNoOffset("views", findPost.getViews(), false, 20, "거짓");
         long resultTime = (System.nanoTime() - startTime) / 1000000;
 
+        System.out.println("lastValue = " + findPost.getViews());
         System.out.println("resultTime(views) = " + resultTime + "ms");
         for (Post post : result) {
             System.out.println("post.getId() = " + post.getId() + " post.getViews() = " + post.getViews());
@@ -88,12 +106,13 @@ class PostRepositoryTest {
 
     private void latestFilterPagingTest(Post findPost) {
         long startTime = System.nanoTime();
-        List<Post> result = postRepository.findAllPostWithNoOffset("latest", findPost.getCreatedAt(), false, 20, null);
+        List<Post> result = postRepository.findAllPostWithNoOffset("latest", findPost.getId(), false, 20, "사랑");
         long resultTime = (System.nanoTime() - startTime) / 1000000;
 
+        System.out.println("lastValue = " + findPost.getId());
         System.out.println("resultTime(latest) = " + resultTime + "ms");
         for (Post post : result) {
-            System.out.println("post.getId() = " + post.getId() + " post.getCreatedAt() = " + post.getCreatedAt());
+            System.out.println("post.getId() = " + post.getId());
         }
     }
 
