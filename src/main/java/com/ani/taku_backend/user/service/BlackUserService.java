ef1a3d580ce9b85cbc61ddde57e6b8972d7b7a50
looco <ses1238@gmail.com>
2024-12-19 16:45:41 +0900

    블랙유저 관련 서비스,jpa 추가

--- src/main/java/com/ani/taku_backend/user/service/BlackUserService.java
+++ src/main/java/com/ani/taku_backend/user/service/BlackUserService.java
@@ -1,8 +1,22 @@
 package com.ani.taku_backend.user.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
+import com.ani.taku_backend.user.model.entity.BlackUser;
+import com.ani.taku_backend.user.repository.BlackUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class BlackUserService {
+
+
+    private final BlackUserRepository blackUserRepository;
+
+    public List<BlackUser> findByUserId(Long userId) {
+        return blackUserRepository.findByUser_Id(userId);
+    }
     
 }
