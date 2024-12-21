ec16e1afac7501cf8e48c26fa4126a82d7306d59
looco <ses1238@gmail.com>
2024-12-21 15:35:34 +0900

    서비스 명칭 변경

--- src/main/java/com/ani/taku_backend/admin/service/ProfanityFilterService.java
+++ src/main/java/com/ani/taku_backend/admin/service/ProfanityFilterService.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class ForbiddenKeywordService {
+public class ProfanityFilterService {
 
     private final ProfanityFilterRepository profanityFilterRepository;
 
