ff3769c3f0e4dd447e461112fb12706d8217ebd9
looco <ses1238@gmail.com>
2024-12-21 18:44:36 +0900

    금칙어 조회 api

--- src/main/java/com/ani/taku_backend/admin/controller/ProfanityController.java
+++ src/main/java/com/ani/taku_backend/admin/controller/ProfanityController.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import com.ani.taku_backend.admin.domain.dto.RequestCreateProfanityDTO;
+import com.ani.taku_backend.admin.domain.dto.RequestSearchProfanityDTO;
 import com.ani.taku_backend.admin.domain.dto.ResponseCreateProfanityDTO;
+import com.ani.taku_backend.admin.domain.dto.ResponseProfannityDTO;
 import com.ani.taku_backend.admin.service.ProfanityFilterService;
 import com.ani.taku_backend.global.response.ApiResponse;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
@@ -30,5 +39,21 @@ public class ProfanityController {
         
         ResponseCreateProfanityDTO profanityFilter = this.profanityFilterService.createProfanityFilter(null, requestCreateProfanityDTO);
         return ApiResponse.created(profanityFilter);
+    }
+
+    /**
+     * 금칙어 필터 목록 조회
+     * @param requestSearchProfanityDTO
+     * @param pageable
+     * @return
+     */
+    @GetMapping("")
+    public ApiResponse<Page<ResponseProfannityDTO>> getProfanityFilters(
+        @ModelAttribute RequestSearchProfanityDTO requestSearchProfanityDTO,
+        @PageableDefault(size = 20, sort = "keyword", direction = Sort.Direction.ASC) Pageable pageable
+
+    ) {
+        Page<ResponseProfannityDTO> profanityFilters = this.profanityFilterService.findProfanityFilterList(requestSearchProfanityDTO, pageable);
+        return ApiResponse.ok(profanityFilters);
     }
 }
--- src/main/java/com/ani/taku_backend/admin/service/ProfanityFilterService.java
+++ src/main/java/com/ani/taku_backend/admin/service/ProfanityFilterService.java
@@ -1,11 +1,20 @@
 package com.ani.taku_backend.admin.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.domain.Specification;
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
 
 import com.ani.taku_backend.admin.domain.dto.RequestCreateProfanityDTO;
+import com.ani.taku_backend.admin.domain.dto.RequestSearchProfanityDTO;
 import com.ani.taku_backend.admin.domain.dto.ResponseCreateProfanityDTO;
+import com.ani.taku_backend.admin.domain.dto.ResponseProfannityDTO;
 import com.ani.taku_backend.admin.domain.entity.ProfanityFilter;
 import com.ani.taku_backend.admin.domain.repository.ProfanityFilterRepository;
 import com.ani.taku_backend.common.annotation.RequireUser;
@@ -49,6 +58,39 @@ public class ProfanityFilterService {
                 .build()
         );
         return ResponseCreateProfanityDTO.of(save);
+    }
+
+    /**
+     * 금칙어 필터 목록 조회
+     * @param requestSearchProfanityDTO
+     * @param pageable
+     * @return
+     */
+    @RequireUser(isAdmin = true)
+    public Page<ResponseProfannityDTO> findProfanityFilterList(RequestSearchProfanityDTO requestSearchProfanityDTO,
+            Pageable pageable) {
+
+        Specification<ProfanityFilter> specification = (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (requestSearchProfanityDTO.getUserName() != null) {
+                predicates.add(criteriaBuilder.like(root.get("admin").get("nickname"), 
+                    "%" + requestSearchProfanityDTO.getUserName() + "%"));
+            }
+            if (requestSearchProfanityDTO.getKeyword() != null) {
+                predicates.add(criteriaBuilder.like(root.get("keyword"), 
+                    "%" + requestSearchProfanityDTO.getKeyword() + "%"));
+            }
+            if (requestSearchProfanityDTO.getExplaination() != null) {
+                predicates.add(criteriaBuilder.like(root.get("explaination"), 
+                    "%" + requestSearchProfanityDTO.getExplaination() + "%"));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+
+        return profanityFilterRepository.findAll(specification, pageable)
+                .map(ResponseProfannityDTO::of);
     }
     
 }
