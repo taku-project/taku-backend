516d91093b57bd4fc0e3e189d1a5788df1710f9c
looco <ses1238@gmail.com>
2024-12-21 15:30:35 +0900

    서비스 수정

--- src/main/java/com/ani/taku_backend/admin/service/ForbiddenKeywordService.java
+++ src/main/java/com/ani/taku_backend/admin/service/ForbiddenKeywordService.java
@@ -2,7 +2,7 @@ package com.ani.taku_backend.admin.service;
 
 import org.springframework.stereotype.Service;
 
-import com.ani.taku_backend.admin.domain.repository.ForbiddenKeywordRepository;
+import com.ani.taku_backend.admin.domain.repository.ProfanityFilterRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ForbiddenKeywordService {
 
-    private final ForbiddenKeywordRepository forbiddenKeywordRepository;
+    private final ProfanityFilterRepository profanityFilterRepository;
 
     
     
