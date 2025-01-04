9b6919d16ebe84c1d4e0e1eb37686c38f782e041
nagul2 <hayani0000@gmail.com>
2025-01-04 10:37:32 +0900

    오류 수정

--- src/main/java/com/ani/taku_backend/post/repository/PostRepository.java
+++ src/main/java/com/ani/taku_backend/post/repository/PostRepository.java
@@ -2,13 +2,7 @@ package com.ani.taku_backend.post.repository;
 
 import com.ani.taku_backend.post.model.entity.Post;
 import com.ani.taku_backend.post.repository.impl.PostRepositoryCustom;
-import com.ani.taku_backend.user.model.entity.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
-    Optional<Post> findPostByUserId(Long userId);
 }
