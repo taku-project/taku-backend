1b24670436528b788ddbccf1c5e2e9b1542be6ad
beetrbgus <devbeetrb0214@gmail.com>
2025-01-07 19:11:24 +0900

    [FEAUTRE] 시청 기록 추가 API

--- src/main/java/com/ani/taku_backend/shorts_interaction/service/InteractionService.java
+++ src/main/java/com/ani/taku_backend/shorts_interaction/service/InteractionService.java
@@ -1,5 +1,6 @@
 package com.ani.taku_backend.shorts_interaction.service;
 
+import com.ani.taku_backend.shorts_interaction.domain.dto.CreateShortsViewDTO;
 import com.ani.taku_backend.user.model.entity.User;
 
 public interface InteractionService {
@@ -7,4 +8,6 @@ public interface InteractionService {
     void addLike(User user, String shortsId);
 
     void cancelLike(User user, String shortsId);
+
+    void createView(CreateShortsViewDTO createShortsViewReqDTO);
 }
