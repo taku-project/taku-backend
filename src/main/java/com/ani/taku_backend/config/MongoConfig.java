4cdb2735cabb1a37f4593fd014e05422461eeca7
nagul2 <hayani0000@gmail.com>
2025-01-11 12:16:54 +0900

    [Setting] 몽고DB class 필드 생성되지 않도록 설정 등록

--- src/main/java/com/ani/taku_backend/config/MongoConfig.java
+++ src/main/java/com/ani/taku_backend/config/MongoConfig.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.convert.*;
 
 import com.ani.taku_backend.config.converter.InteractionTypeConverter;
 import com.ani.taku_backend.config.converter.StringToInteractionTypeConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 @EnableMongoAuditing
@@ -31,5 +31,16 @@ public class MongoConfig {
             new InteractionTypeConverter(),
             new StringToInteractionTypeConverter()
         ));
+    }
+
+    /**
+     * 몽고 DB의 class 필드 제거
+     */
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDbFactory, MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return mappingConverter;
     }
 }
