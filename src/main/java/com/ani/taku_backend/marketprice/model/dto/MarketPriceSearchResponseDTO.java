9cc46dc4cb1d709a52b18996f39635a89edb3161
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-01-13 22:36:14 +0900

    refactor: Jackson 관련 어노테이션 모두 제거

--- src/main/java/com/ani/taku_backend/marketprice/model/dto/MarketPriceSearchResponseDTO.java
+++ src/main/java/com/ani/taku_backend/marketprice/model/dto/MarketPriceSearchResponseDTO.java
@@ -1,15 +1,14 @@
 package com.ani.taku_backend.marketprice.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+@Builder
+@Schema(description = "시세 검색 응답 DTO")
 public class MarketPriceSearchResponseDTO {
 
     @Schema(description = "검색 키워드", example = "원피스 루피 피규어")
@@ -24,13 +23,11 @@ public class MarketPriceSearchResponseDTO {
     @Schema(description = "유사 상품 목록")
     private final List<SimilarProductResponseDTO> similarProducts;
 
-    @JsonCreator
-    @Builder
     public MarketPriceSearchResponseDTO(
-            @JsonProperty("keyword") String keyword,
-            @JsonProperty("priceGraph") PriceGraphResponseDTO priceGraph,
-            @JsonProperty("weeklyStats") WeeklyStatsResponseDTO weeklyStats,
-            @JsonProperty("similarProducts") List<SimilarProductResponseDTO> similarProducts
+            String keyword,
+            PriceGraphResponseDTO priceGraph,
+            WeeklyStatsResponseDTO weeklyStats,
+            List<SimilarProductResponseDTO> similarProducts
     ) {
         this.keyword = keyword;
         this.priceGraph = priceGraph;
--- src/main/java/com/ani/taku_backend/marketprice/model/dto/PriceGraphRequestDTO.java
+++ src/main/java/com/ani/taku_backend/marketprice/model/dto/PriceGraphRequestDTO.java
@@ -1,7 +1,7 @@
 package com.ani.taku_backend.marketprice.model.dto;
 
 import com.ani.taku_backend.marketprice.model.constant.GraphDisplayOption;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -16,18 +16,22 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+@Schema(description = "시세 그래프 요청 DTO")
 public class PriceGraphRequestDTO {
 
+    @Schema(description = "검색 키워드", example = "원피스 루피 피규어")
     @NotBlank(message = "키워드는 필수입니다")
     private String keyword;
 
+    @Schema(description = "조회 시작일", example = "2024-01-01")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fromDate;
 
+    @Schema(description = "조회 종료일", example = "2024-01-31")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate toDate;
 
+    @Schema(description = "그래프 표시 옵션", example = "ALL")
     private GraphDisplayOption displayOption;
 
     @Override
