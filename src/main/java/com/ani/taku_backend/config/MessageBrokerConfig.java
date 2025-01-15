39db8d77f667bc94f176b465f324d91dba50a6f1
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-01-15 13:54:37 +0900

    refacor: 채팅 config관련  class 이름 변경

--- src/main/java/com/ani/taku_backend/config/MessageBrokerConfig.java
+++ src/main/java/com/ani/taku_backend/config/MessageBrokerConfig.java
@@ -1,5 +1,6 @@
 package com.ani.taku_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,32 +10,29 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class ChattingConfig implements WebSocketMessageBrokerConfigurer {
+public class MessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
-    private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:3000";
+    @Value("${client.front-url}")
+    private String frontUrl;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // stomp 접속 주소 url = ws://localhost:8080/ws, 프로토콜이 http가 아님
-        registry.addEndpoint("/ws")  // 연결될 엔드포인트
-                .setAllowedOrigins(DEVELOP_FRONT_ADDRESS)
+        registry.addEndpoint("/ws")  // WebSocket 엔드포인트
+                .setAllowedOrigins(frontUrl)
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 메시지를 구독(수신)하는 요청 엔드포인트
+        // 채팅, 알림 등 실시간 메시지를 위한 브로커 설정
         registry.enableSimpleBroker("/sub");
-
-        // 메시지를 발행(송신)하는 엔드포인트
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
-    //TODO 협의후 수정 가능
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(160 * 64 * 1024)    // 메시지 크기 제한
-                .setSendTimeLimit(20 * 10000)             // 전송 시간 제한
-                .setSendBufferSizeLimit(3 * 512 * 1024);  // 버퍼 크기 제한
+        registration.setMessageSizeLimit(160 * 64 * 1024)
+                .setSendTimeLimit(20 * 10000)
+                .setSendBufferSizeLimit(3 * 512 * 1024);
     }
 }
