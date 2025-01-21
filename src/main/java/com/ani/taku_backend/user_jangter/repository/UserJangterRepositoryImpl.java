db3fd9e6122a186d7a7677557aeecb869ffc8e1b
beetrbgus <devbeetrb0214@gmail.com>
2025-01-21 21:59:40 +0900

    [STYLE] import문 수정

--- src/main/java/com/ani/taku_backend/user_jangter/repository/UserJangterRepositoryImpl.java
+++ src/main/java/com/ani/taku_backend/user_jangter/repository/UserJangterRepositoryImpl.java
@@ -1,7 +1,8 @@
 package com.ani.taku_backend.user_jangter.repository;
 
 import com.ani.taku_backend.user_jangter.domain.CompleteJangterSortType;
-import com.ani.taku_backend.user_jangter.dto.QUserPurchaseResponseDTO;
+import com.ani.taku_backend.user_jangter.dto.res.QUserCellResponseDTO;
+import com.ani.taku_backend.user_jangter.dto.res.QUserPurchaseResponseDTO;
 import com.ani.taku_backend.user_jangter.dto.res.UserCellResponseDTO;
 import com.ani.taku_backend.user_jangter.dto.res.UserPurchaseResponseDTO;
 import com.querydsl.core.types.Order;
