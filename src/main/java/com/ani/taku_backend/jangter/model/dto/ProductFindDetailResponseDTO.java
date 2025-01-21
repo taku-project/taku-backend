1c0ee4e6d55a7ef0f55a7778b6699e4014db195c
nagul2 <hayani0000@gmail.com>
2025-01-21 17:01:12 +0900

    [Feature] 장터 상세조회 응답값 수정

--- src/main/java/com/ani/taku_backend/jangter/model/dto/ProductFindDetailResponseDTO.java
+++ src/main/java/com/ani/taku_backend/jangter/model/dto/ProductFindDetailResponseDTO.java
@@ -49,6 +49,7 @@ public class ProductFindDetailResponseDTO {
         this.price = duckuJangter.getPrice();
         this.status = duckuJangter.getStatus();
         this.createdAt = duckuJangter.getCreatedAt();
+        this.itemCategoryId = duckuJangter.getId();
         this.userId = duckuJangter.getUser().getUserId();
         this.viewCount = addViewCount;
 
