32f6dd9a857306ccd86898dc95200598f063f120
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-01-29 01:14:21 +0900

    chore: 불필요한 import 문 삭제

--- src/main/java/com/ani/taku_backend/post/controller/PostController.java
+++ src/main/java/com/ani/taku_backend/post/controller/PostController.java
@@ -17,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
