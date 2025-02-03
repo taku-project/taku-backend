e08785e19c32b5021d60d7b2b93bf38ae58dcfab
beetrbgus <devbeetrb0214@gmail.com>
2025-02-03 19:49:49 +0900

    [FEATURE] 카테고리 북마크 추가

--- src/main/java/com/ani/taku_backend/common/exception/ErrorCode.java
+++ src/main/java/com/ani/taku_backend/common/exception/ErrorCode.java
@@ -75,7 +75,12 @@ public enum ErrorCode {
     NOT_FOUND_INTERACTION(40408, HttpStatus.NOT_FOUND, "존재하지 않는 상호작용입니다."),
     // Chat
     DUPLICATE_CHAT_ROOM(40903, HttpStatus.CONFLICT, "이미 존재하는 채팅방입니다."),
-    CHAT_ROOM_NOT_FOUND(40407, HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다.");
+    CHAT_ROOM_NOT_FOUND(40407, HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다."),
+
+    // Category Bookmark
+    NOT_FOUND_CATEGORY_BOOKMARK(40409, HttpStatus.NOT_FOUND, "존재하지 않는 카테고리 북마크입니다."),
+    ;
+
     // 403 Forbidden
     private final Integer code;
     private final HttpStatus httpStatus;
