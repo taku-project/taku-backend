06876cfa7941a9dd0cace06a4c0551fc909327bc
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-02-19 01:50:56 +0900

    chore: Remove bookmark and jangter files from repository

--- src/main/java/com/ani/taku_backend/jangter/repository/JangterRankTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ani.taku_backend.jangter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ani.taku_backend.jangter.model.entity.rank.JangterRankType;
-
-public interface JangterRankTypeRepository extends JpaRepository<JangterRankType, Long> {
-    
-}
--- src/main/java/com/ani/taku_backend/jangter/score/ScoreCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.ani.taku_backend.jangter.score;
-
-import com.ani.taku_backend.jangter.model.entity.DuckuJangter;
-import java.util.List;
-
-public interface ScoreCalculator<T> {
-    double calculate(DuckuJangter recommendProduct, List<String> recommendProductKeywords, T userHistory);
-}
