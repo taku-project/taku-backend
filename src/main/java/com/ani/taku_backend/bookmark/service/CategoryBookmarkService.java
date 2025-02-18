3f8a3be170926bdd0d6cd6597a9c6f569cc54998
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-02-19 01:24:20 +0900

    chore: 북마크 관련 파일들 삭제

--- src/main/java/com/ani/taku_backend/bookmark/service/CategoryBookmarkService.java
+++
@@ -1,9 +0,0 @@
-package com.ani.taku_backend.bookmark.service;
-
-import com.ani.taku_backend.user.model.entity.User;
-
-public interface CategoryBookmarkService {
-    void createCategoryBookmark(User user, Long categoryId);
-
-    void deleteCategoryBookmark(User user, Long categoryId);
-}
