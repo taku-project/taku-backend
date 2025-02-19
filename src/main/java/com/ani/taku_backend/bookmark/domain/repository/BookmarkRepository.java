0a1d6bff0e9590c71d10aea13c20585233144e2b
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-02-19 10:55:43 +0900

    chore: cherry pick 중에 중복되는 파일 제거

--- src/main/java/com/ani/taku_backend/bookmark/domain/repository/BookmarkRepository.java
+++ src/main/java/com/ani/taku_backend/bookmark/domain/repository/BookmarkRepository.java
@@ -11,11 +11,11 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     @Query("select b from Bookmark b where b.user.id = :userId")
     List<Bookmark> findByUserId(@Param("userId") Long userId);
-    
+
     @Query("SELECT b FROM Bookmark b " +
-       "JOIN FETCH b.duckuJangterBookmarks db " +
-       "JOIN FETCH db.jangter j " +
-       "JOIN FETCH j.itemCategories " +
-       "WHERE b.user.id = :userId")
+            "JOIN FETCH b.duckuJangterBookmarks db " +
+            "JOIN FETCH db.jangter j " +
+            "JOIN FETCH j.itemCategories " +
+            "WHERE b.user.id = :userId")
     List<Bookmark> findByUserIdWithJangterAndCategories(@Param("userId") Long userId);
 }
